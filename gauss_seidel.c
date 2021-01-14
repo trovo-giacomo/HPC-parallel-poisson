e739c024f87932ce86c4612d73d547d61baa7b74
KathGran8 <akgm.nu@gmail.com>
2021-01-14 14:09:13 +0100

    fix GS (user time 4x real time)

--- gauss_seidel.c
+++ gauss_seidel.c
@@ -3,8 +3,7 @@
  */
 #include <math.h>
 #include <time.h>
-#define mytimer clock
-#define delta_t(a,b) (1e3 * (b - a) / CLOCKS_PER_SEC)
+#include <omp.h>
 
 
 int gauss_seidel(double ***u, double ***f, int N, int max_iter, double *threshold) {
@@ -12,32 +11,28 @@ int gauss_seidel(double ***u, double ***f, int N, int max_iter, double *threshol
     double norm = 1000.0; 
     double h = 1.0/6.0;
     double delta_2 = 4.0/(N*N), error;
-    clock_t t1 ,t2, total;
 
-    t1 = mytimer();
-
-    while(it < max_iter && norm > *threshold){
-        error = 0.0;
+    while(it < max_iter){ // && norm > *threshold){
+        //error = 0.0;
         // update
-        #pragma omp parallel for ordered(2) 
-        for(int i=1; i<N-1; i++){
-            for(int j=1; j<N-1; j++){
+        int i,j,k;
+        #pragma omp parallel for ordered(2) schedule(static,1) default(none) shared(u,N,delta_2,h,f,i) private(j,k)//reduction(+: error)
+        for(i=1; i<N-1; i++){
+            for(j=1; j<N-1; j++){
                 #pragma omp ordered depend(sink:i-1, j) depend(sink:i, j-1)
-                for(int k=1; k<N-1; k++){
-                    double u_old =  u[i][j][k];
+                for(k=1; k<N-1; k++){
+                    //double u_old =  u[i][j][k];
                     u[i][j][k] = h*(u[i-1][j][k]+u[i+1][j][k] + u[i][j-1][k]+u[i][j+1][k] + u[i][j][k-1] +u[i][j][k+1] + delta_2*f[i][j][k]);
 
-                    error += (u[i][j][k] - u_old) * (u[i][j][k] - u_old);
+                    //error += (u[i][j][k] - u_old) * (u[i][j][k] - u_old);
                 }
                 #pragma omp ordered depend(source)
             }
         }
-        norm = sqrt(error);
+        //norm = sqrt(error);
         it++;
     }
-    t2 = mytimer();
-    total = delta_t(t1,t2);
-    //printf("%f\n",total);
+    
 
     *threshold = norm;
     return it;
